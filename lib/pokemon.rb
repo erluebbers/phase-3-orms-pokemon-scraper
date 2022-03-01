9bdf6c7a34f76bff94bf057d6d02b6899d4d17f5
Erik Luebbers <erluebbers@gmail.com>
2022-03-01 07:23:37 -0800

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,11 +11,11 @@ class Pokemon
 
   def self.save(name, type, db)
     sql = <<-SQL
-      INSERT INTO pokemon (name, type)
-      VALUES (?, ?)
+      INSERT INTO pokemon (name, type, db)
+      VALUES (?, ?, ?)
     SQL
-    # DB[:conn].execute(sql, self.name, self.type, self.db)
-    # self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    db.execute(sql, self.name, self.type, self.db)
+    self.id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
   end
 
 
